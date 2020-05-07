69aa3dc52ed0e3138e1ce8b157ef80f244544a15
Evgeny Pryakhin <pryakhin.e@sberbank.ru>
2020-05-07 18:11:50 +0300

    Добавлена отладка в консоль

--- complete/src/main/java/com/example/consumingrest/clienttest/ABUnsignedHash.java
+++ complete/src/main/java/com/example/consumingrest/clienttest/ABUnsignedHash.java
@@ -6,6 +6,7 @@ import org.bouncycastle.util.BigIntegers;
 import org.bouncycastle.util.encoders.Hex;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class ABUnsignedHash {
 
@@ -15,16 +16,25 @@ public class ABUnsignedHash {
     public static void main(String[] args) {
         final Digest sha3_512 = DigestFactory.createSHA3_512();
 
+        System.out.println("\n---\nA\n---");
         appendHexToDigest(sha3_512, A_HEX);
+        System.out.println("---\nB\n---");
         appendHexToDigest(sha3_512, B_HEX);
 
         final byte[] result = new byte[sha3_512.getDigestSize()];
         sha3_512.doFinal(result, 0);
+
+        System.out.println();
         System.out.println(Hex.toHexString(result));
     }
 
     private static void appendHexToDigest(Digest digest, String hex) {
-        byte[] bytes = BigIntegers.asUnsignedByteArray(new BigInteger(1, Hex.decode(hex)));
-        digest.update(bytes, 0, bytes.length);
+        byte[] bigIntBytes = Hex.decode(hex);
+        System.out.println("bigIntBytes: " + Arrays.toString(bigIntBytes));
+        BigInteger bigInt = new BigInteger(1, bigIntBytes);
+        System.out.println("bigInt: " + bigInt);
+        byte[] unsignedBytes = BigIntegers.asUnsignedByteArray(bigInt);
+        System.out.println("unsigned unsignedBytes: " + Arrays.toString(unsignedBytes));
+        digest.update(unsignedBytes, 0, unsignedBytes.length);
     }
 }
